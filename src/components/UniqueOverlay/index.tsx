d6e9c8e60d747eab21600226a6b6e725b20aaba8
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-12-27 18:22:57 -0300

    Header & Footer

---
+++ src/components/UniqueOverlay/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useTransform } from 'framer-motion';
+
+import useWrapperScroll from '../Model/useWrapperScroll';
+
+import { Container, Header, Logo, Burguer, Footer } from './styles';
+
+const UniqueOverlay: React.FC = () => {
+  const { scrollYProgress } = useWrapperScroll()
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1])
+
+  return (
+    <Container>
+      <Header>
+        <Logo />
+
+        <Burguer />
+      </Header>
+
+      <Footer style={{ opacity }}>
+        <ul>
+          <li>
+            <a href="#">UI Clone</a>
+          </li>
+
+          <li>
+            <a href="#">Made with 💚</a>
+          </li>
+
+          <li>
+            <a href="#">By Leonardo Campello</a>
+          </li>
+        </ul>
+      </Footer>
+    </Container>
+  );
+};
+
+export default UniqueOverlay;
